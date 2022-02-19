968b19d88c22b5f3b9419589b16a39955c46438e
Roman Naumenko <hi@catenary.cloud>
2022-02-19 14:22:12 -0500

    Format code

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { App, CfnOutput, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { producerStack } from "./producer";
 import { IBucket } from "aws-cdk-lib/aws-s3";
-import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 
 interface consumerStackProps extends StackProps {
   readonly bucket: IBucket;
@@ -11,12 +11,12 @@ interface consumerStackProps extends StackProps {
 export class consumerStack extends Stack {
   constructor(scope: Construct, id: string, props: consumerStackProps) {
     super(scope, id, props);
-  
-    new BucketDeployment(this, 'uploadFile', {
-      sources: [Source.asset('./src')],
-      destinationBucket: props.bucket
+
+    new BucketDeployment(this, "uploadFile", {
+      sources: [Source.asset("./src")],
+      destinationBucket: props.bucket,
     });
-    new CfnOutput(this, 'bucketName', { value: props.bucket.bucketName });
+    new CfnOutput(this, "bucketName", { value: props.bucket.bucketName });
   }
 }
 
--- src/producer.ts
+++ src/producer.ts
@@ -1,13 +1,13 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { IBucket, Bucket } from 'aws-cdk-lib/aws-s3';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { IBucket, Bucket } from "aws-cdk-lib/aws-s3";
 
 export class producerStack extends Stack {
   public readonly bucket: IBucket;
-  
+
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    this.bucket = new Bucket(this, 'Bucket');
+    this.bucket = new Bucket(this, "Bucket");
   }
 }
